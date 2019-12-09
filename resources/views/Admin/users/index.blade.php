04a262e4e21d3819346efcefa49081e31343c1fd
Pouyan Sodagari <pouyan.sodagari@gmail.com>
2019-12-09 16:37:22 +0100

    users edit is done except security

--- resources/views/Admin/users/index.blade.php
+++ resources/views/Admin/users/index.blade.php
@@ -7,10 +7,10 @@
         <tr>
             <th scope="col">Id</th>
             <th scope="col">Name</th>
+            <th scope="col">Image</th>
             <th scope="col">Email</th>
             <th scope="col">Role</th>
             <th scope="col">Status</th>
-            <th scope="col">Image</th>
             <th scope="col">Created</th>
             <th scope="col">Updated</th>
         </tr>
@@ -20,11 +20,14 @@
             @foreach($users as $user)
                 <tr>
                     <th scope="row">{{ $user->id }}</th>
-                    <td>{{ $user->name }}</td>
+                    <td><a href="{{ route('users.edit', $user->id) }}">{{ $user->name }}</a></td>
+                    <td>
+                        <img height="100" width="100" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/100x100'}}" alt=""
+                             class="img-responsive img-rounded">
+                    </td>
                     <td>{{ $user->email }}</td>
                     <td>{{ $user->role->name ?? ''}}</td>
                     <td>{{ $user->is_active == 1 ? 'Active' : 'Not Active' }}</td>
-{{--                    <td>{{ $user->images->file }}</td>--}}
                     <td>{{ $user->created_at->diffForHumans() }}</td>
                     <td>{{ $user->updated_at->diffForHumans() }}</td>
                 </tr>
